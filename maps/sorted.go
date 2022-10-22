dfa3097d4373dde9a9f1868b5de8b26a53aee826
Alex Denisov <ad@tessonics.com>
2022-10-22 13:18:24 -0400

    adding maps.SortedKeysFunc

--- maps/sorted.go
+++ maps/sorted.go
@@ -13,3 +13,12 @@ func SortedKeys[M ~map[K]V, K constraints.Ordered, V any](m M) []K {
 	slices.Sort(keys)
 	return keys
 }
+
+// SortedKeysFunc returns sorted keys of the map m as determined by the less
+// function. This sort is stable provided the less function produces stable
+// results.
+func SortedKeysFunc[M ~map[K]V, K comparable, V any](m M, less func(a, b K) bool) []K {
+	keys := maps.Keys(m)
+	slices.SortFunc(keys, less)
+	return keys
+}
