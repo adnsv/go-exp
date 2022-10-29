28c5083dc75f82a3bff22138f36d130ed665d2dd
Alex Denisov <ad@tessonics.com>
2022-10-29 13:37:55 -0400

    added maps.EqualKeys

--- maps/keys.go
+++ maps/keys.go
@@ -19,3 +19,16 @@ func Sliced[M ~map[K]V, S ~map[K]struct{}, K comparable, V any](m M, s S) M {
 	}
 	return r
 }
+
+// Equal reports whether two maps contain the same keys.
+func EqualKeys[M1 ~map[K]any, M2 ~map[K]any, K comparable](m1 M1, m2 M2) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k := range m1 {
+		if _, ok := m2[k]; !ok {
+			return false
+		}
+	}
+	return true
+}
